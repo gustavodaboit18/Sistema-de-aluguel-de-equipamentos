9b47e329f0c85de6bae074d0b3923d5e176a236a
Isabela Zanette Martinello <110418228+IsabelaZanette@users.noreply.github.com>
2025-11-28 21:01:30 -0300

    Update perguntas_de_negocio.sql

--- docker/sql/perguntas_de_negocio.sql
+++ docker/sql/perguntas_de_negocio.sql
@@ -1,4 +1,4 @@
---pergunta 1: 1-Em qual cliente est· cada equipamento? JOIN
+--pergunta 1: 1-Em qual cliente est√° cada equipamento? JOIN
 
 
 SELECT e.nome,e.marca,e.id_equipamento ,c.nome
@@ -7,7 +7,7 @@ inner join locacao_itens li on li.id_equipamento=e.id_equipamento
 inner join locacao l on l.id_locacao=li.id_locacao
 inner join cliente c on c.id_cliente=l.id_cliente
 
--- pergunta 2-Qual os 5 equipamentos que teve mais aluguÈis? CTE
+-- pergunta 2-Qual os 5 equipamentos que teve mais alugu√©is? CTE
 
 with cte as(
 select e.nome,e.marca,li.id_equipamento,count(li.id_locacao) as qtde_locacoes
@@ -21,6 +21,29 @@ from cte
 select * from ranking
 where rank<=5
 order by qtde_locacoes desc
+
+-- pergunta 4- Quais os tr√™s equipamentos que geraram mais receita para a empresa em um determinado ano? FUNCTION
+
+DROP FUNCTION IF EXISTS dbo.fn_EquipamentosReceita;
+GO
+CREATE FUNCTION dbo.fn_EquipamentosReceita (@Ano INT)
+RETURNS TABLE
+AS
+RETURN (
+    SELECT TOP 3 
+        li.id_equipamento AS id_equip,
+        SUM(e.preco) AS receita
+    FROM locacao_itens li
+    INNER JOIN equipamento e ON e.id_equipamento = li.id_equipamento
+    WHERE 
+        li.data_inicio >= DATEFROMPARTS(@Ano, 1, 1)
+        AND li.data_inicio < DATEFROMPARTS(@Ano + 1, 1, 1)
+    GROUP BY li.id_equipamento
+    ORDER BY receita DESC
+);
+GO
+
+select * from dbo.fn_EquipamentosReceita(2023);
 
 
 
