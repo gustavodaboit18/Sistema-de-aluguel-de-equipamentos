8d2412c8c8066c0956c24d86899c2f8882d1fa4c
igormatos99 <igorhu3asd@gmail.com>
2025-12-01 13:29:48 -0300

    Update estoque.sql

--- scripts/ddl/tabelas/estoque.sql
+++ scripts/ddl/tabelas/estoque.sql
@@ -1,1 +1,11 @@
--- vazio
+DROP TABLE IF EXISTS estoque;
+GO
+
+CREATE TABLE estoque (
+    id_estoque INT IDENTITY(1,1) PRIMARY KEY,
+    id_equipamento INT NOT NULL,
+    tipo BIGINT NOT NULL,
+    qtde_disponivel BIGINT NOT NULL,
+    FOREIGN KEY (id_equipamento) REFERENCES equipamento(id_equipamento)
+);
+GO
