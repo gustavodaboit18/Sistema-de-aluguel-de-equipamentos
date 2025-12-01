6b8ff7c5f3e74bd2058719c1d1a13f6ec9a0ed4c
igormatos99 <igorhu3asd@gmail.com>
2025-12-01 13:32:28 -0300

    Update manutencao.sql

--- scripts/ddl/tabelas/manutencao.sql
+++ scripts/ddl/tabelas/manutencao.sql
@@ -1,1 +1,14 @@
--- vazio
+DROP TABLE IF EXISTS manutencao;
+GO
+
+CREATE TABLE manutencao (
+    id_manutencao INT IDENTITY(1,1) PRIMARY KEY,
+    id_equipamento INT NOT NULL,
+    tipo VARCHAR(15) NOT NULL,
+    descricao VARCHAR(200) NOT NULL,
+    data_entrada DATE NOT NULL,
+    data_saida DATE NOT NULL,
+    custo DECIMAL(10, 2) NOT NULL,
+    FOREIGN KEY (id_equipamento) REFERENCES equipamento(id_equipamento)
+);
+GO
