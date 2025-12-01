b0a389db8bd8f636d1d578c76210816f722e9213
igormatos99 <igorhu3asd@gmail.com>
2025-12-01 13:29:11 -0300

    Update cliente.sql

--- scripts/ddl/tabelas/cliente.sql
+++ scripts/ddl/tabelas/cliente.sql
@@ -1,1 +1,14 @@
--- vazio
+DROP TABLE IF EXISTS cliente;
+GO
+
+CREATE TABLE cliente (
+    id_cliente INT IDENTITY(1,1) PRIMARY KEY,
+    nome VARCHAR(80) NOT NULL,
+    cpf CHAR(11) NOT NULL,
+    email VARCHAR(80) NOT NULL,
+    telefone VARCHAR(15) NOT NULL,
+    cnpj CHAR(14) NOT NULL,
+    tipo VARCHAR(5) NOT NULL,
+    nome_responsavel VARCHAR(80) NOT NULL
+);
+GO
