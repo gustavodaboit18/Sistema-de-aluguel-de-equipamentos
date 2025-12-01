08c06f8fe592068ff30a0b3e61987b29f97dd7a7
igormatos99 <igorhu3asd@gmail.com>
2025-12-01 13:31:55 -0300

    Update log_entradasaida.sql

--- scripts/ddl/tabelas/log_entradasaida.sql
+++ scripts/ddl/tabelas/log_entradasaida.sql
@@ -1,1 +1,12 @@
--- vazio
+DROP TABLE IF EXISTS log_entradasaida;
+GO
+
+CREATE TABLE log_entradasaida (
+    id_log INT IDENTITY(1,1) PRIMARY KEY,
+    id_equipamento INT NOT NULL,
+    tipo CHAR(1) NOT NULL,
+    data DATETIME NOT NULL,
+    quantidade INT NOT NULL,
+    FOREIGN KEY (id_equipamento) REFERENCES equipamento(id_equipamento)
+);
+GO
