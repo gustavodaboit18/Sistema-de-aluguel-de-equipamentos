4c2f61a20ac18c82b7023e657eda088a2361b57c
igormatos99 <igorhu3asd@gmail.com>
2025-12-01 13:30:49 -0300

    Update locacao.sql

--- scripts/ddl/tabelas/locacao.sql
+++ scripts/ddl/tabelas/locacao.sql
@@ -1,1 +1,10 @@
--- vazio
+DROP TABLE IF EXISTS locacao;
+GO
+
+CREATE TABLE locacao (
+    id_locacao INT IDENTITY(1,1) PRIMARY KEY,
+    id_cliente INT NOT NULL,
+    data_solicitacao DATE NOT NULL,
+    FOREIGN KEY (id_cliente) REFERENCES cliente(id_cliente)
+);
+GO
