0738fb6a36c8530919bd5a3b964c87f8d97eb7e6
igormatos99 <igorhu3asd@gmail.com>
2025-12-01 13:28:30 -0300

    Update equipamentos.sql

--- scripts/ddl/tabelas/equipamentos.sql
+++ scripts/ddl/tabelas/equipamentos.sql
@@ -1,1 +1,13 @@
--- vazio
+DROP TABLE IF EXISTS equipamento;
+GO
+
+CREATE TABLE equipamento (
+    id_equipamento INT IDENTITY(1,1) PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL,
+    preco DECIMAL(10, 2) NOT NULL,
+    marca VARCHAR(30) NOT NULL,
+    numero_de_serie VARCHAR(20) NOT NULL,
+    data_compra DATE NOT NULL
+);
+GO
+
