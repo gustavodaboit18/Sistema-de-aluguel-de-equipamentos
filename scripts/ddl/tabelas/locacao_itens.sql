6038ce6d30da22f7eb637c7e7aa9db3183513fbe
igormatos99 <igorhu3asd@gmail.com>
2025-12-01 13:31:12 -0300

    Update locacao_itens.sql

--- scripts/ddl/tabelas/locacao_itens.sql
+++ scripts/ddl/tabelas/locacao_itens.sql
@@ -1,1 +1,14 @@
--- vazio
+DROP TABLE IF EXISTS locacao_itens;
+GO
+
+CREATE TABLE locacao_itens (
+    id_item INT IDENTITY(1,1) PRIMARY KEY,
+    id_locacao INT NOT NULL,
+    id_equipamento INT NOT NULL,
+    data_solicitacao DATE NOT NULL,
+    data_inicio DATE NOT NULL,
+    data_fim DATE NOT NULL,
+    FOREIGN KEY (id_locacao) REFERENCES locacao(id_locacao),
+    FOREIGN KEY (id_equipamento) REFERENCES equipamento(id_equipamento)
+);
+GO
