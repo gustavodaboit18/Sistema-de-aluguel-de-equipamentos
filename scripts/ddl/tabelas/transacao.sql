4502f5308ba918a6e87c4b7cf5b4c6e4206e46dc
igormatos99 <igorhu3asd@gmail.com>
2025-12-01 13:33:02 -0300

    Update transacao.sql

--- scripts/ddl/tabelas/transacao.sql
+++ scripts/ddl/tabelas/transacao.sql
@@ -1,1 +1,13 @@
--- vazio
+DROP TABLE IF EXISTS transacao;
+GO
+
+CREATE TABLE transacao (
+    id_transacao INT IDENTITY(1,1) PRIMARY KEY,
+    id_locacao INT NOT NULL,
+    id_manutencao INT NOT NULL,
+    tipo VARCHAR(10) NOT NULL,
+    data_retorno DATE NOT NULL,
+    FOREIGN KEY (id_locacao) REFERENCES locacao(id_locacao),
+    FOREIGN KEY (id_manutencao) REFERENCES manutencao(id_manutencao)
+);
+GO
